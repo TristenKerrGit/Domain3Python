f6794a9a151f1f70d3c586ce764bbd11c4e0ef47
TristenKerrGit <tristenkerr57@gmail.com>
2026-09-23 09:43:05 -0500

    .

---
+++ 315-append-completed.py
@@ -0,0 +1,13 @@
+message = open('315-message.txt','w')
+message.write('Testing file for player configuration\n')
+message.write('Testing file for player score\n')
+message.close()
+
+message = open('315-message.txt','a')
+message.write("Spencer\n")
+message.close()
+
+message = open('315-message.txt', 'r')
+content = message.read()
+print(content)
+message.close()
